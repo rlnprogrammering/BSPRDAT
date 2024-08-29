41951e981b345bbf7f0beff81aa5381e34f986b7
Rasmus Nielsen <rlnprogrammering@gmail.com>
2024-08-29 13:49:26 +0200

    java parser (i)

--- Intro/SimpleExpr.java
+++ Intro/SimpleExpr.java
@@ -18,7 +18,11 @@ class CstI extends Expr {
 
   public int eval(Map<String,Integer> env) {
     return i;
-  } 
+  }
+
+  public String toString(){
+    return "" + i;
+  }
 }
 
 class Var extends Expr { 
@@ -31,9 +35,14 @@ class Var extends Expr {
   public int eval(Map<String,Integer> env) {
     return env.get(name);
   } 
+
+  public String toString(){
+    return name;
+  }
+
 }
 
-class Prim extends Expr { 
+class Prim extends Expr { //  We expect this to be Binop according to the task
   protected final String oper;
   protected final Expr e1, e2;
 
@@ -50,8 +59,36 @@ class Prim extends Expr {
       return e1.eval(env) - e2.eval(env);
     else
       throw new RuntimeException("unknown primitive");
-  } 
+  }
+
+
+ public String toString() {
+  return e1 + " " + oper + " " + e2;
+ }
 }
+
+class Add extends Prim {
+ public Add(Expr e1, Expr e2) {
+  super("+", e1, e2);
+ }
+}
+
+class Sub extends Prim {
+ public Sub(Expr e1, Expr e2) {
+  super("-", e1, e2);
+ }
+}
+
+class Mul extends Prim {
+ public Mul(Expr e1, Expr e2) {
+  super("*", e1, e2);
+ }
+}
+
+
+
+
+
 
 public class SimpleExpr {
   public static void main(String[] args) {
@@ -64,8 +101,22 @@ public class SimpleExpr {
     env0.put("c", 78);
     env0.put("baf", 666);
     env0.put("b", 111);
+
     System.out.println(e1.eval(env0));
     System.out.println(e2.eval(env0));
     System.out.println(e3.eval(env0));
+
+    // Excercise 1.4 (i):
+    Expr e4 = new Add(new CstI(17), new Var("z"));
+
+    // Excercise 1.4 (ii):
+    Expr e5 = new Sub(new CstI(17), new Add(new CstI(12), new Var("x")));
+    Expr e6 = new Mul(new CstI(17), new Var("z"));
+    Expr e7 = new Mul(new CstI(17), new Add(new CstI(32), new CstI(24)));
+
+    System.out.println(e4.toString());
+    System.out.println(e5.toString());
+    System.out.println(e6.toString());
+    System.out.println(e7.toString());
   }
 }
