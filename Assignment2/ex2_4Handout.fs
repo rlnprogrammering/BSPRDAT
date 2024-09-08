240d1a6bed8141f52f8ddd710b5c40ddc6a0ca2e
Rasmus Nielsen <rlnprogrammering@gmail.com>
2024-09-08 13:39:46 +0200

    Exercise 2.4 done

--- Assignment2/ex2_4Handout.fs
+++ Assignment2/ex2_4Handout.fs
@@ -1,4 +1,41 @@
 
+type sinstr =
+  | SCstI of int                        (* push integer           *)
+  | SVar of int                         (* push variable from env *)
+  | SAdd                                (* pop args, push sum     *)
+  | SSub                                (* pop args, push diff.   *)
+  | SMul                                (* pop args, push product *)
+  | SPop                                (* pop value/unbind var   *)
+  | SSwap;;                             (* exchange top and next  *)
+
+type stackvalue =
+  | Value                               (* A computed value *)
+  | Bound of string;; 
+ 
+type expr = 
+  | CstI of int
+  | Var of string
+  | Let of string * expr * expr
+  | Prim of string * expr * expr;;
+
+let rec getindex vs x = 
+    match vs with 
+    | []    -> failwith "Variable not found"
+    | y::yr -> if x=y then 0 else 1 + getindex yr x;;
+
+let rec scomp (e : expr) (cenv : stackvalue list) : sinstr list =
+    match e with
+    | CstI i -> [SCstI i]
+    | Var x  -> [SVar (getindex cenv (Bound x))]
+    | Let(x, erhs, ebody) -> 
+          scomp erhs cenv @ scomp ebody (Bound x :: cenv) @ [SSwap; SPop]
+    | Prim("+", e1, e2) -> 
+          scomp e1 cenv @ scomp e2 (Value :: cenv) @ [SAdd] 
+    | Prim("-", e1, e2) -> 
+          scomp e1 cenv @ scomp e2 (Value :: cenv) @ [SSub] 
+    | Prim("*", e1, e2) -> 
+          scomp e1 cenv @ scomp e2 (Value :: cenv) @ [SMul] 
+    | Prim _ -> failwith "scomp: unknown operator";;
 
 (* Ex 2.4 - assemble to integers *)
 (* SCST = 0, SVAR = 1, SADD = 2, SSUB = 3, SMUL = 4, SPOP = 5, SSWAP = 6; *)
@@ -11,16 +48,21 @@ let sinstrToInt = function
   | SPop    -> [5]
   | SSwap   -> [6]
 
-let assemble instrs = instrs |> List.fold (fun acc sinstr -> 
-    match sinstr with 
-    | 0 -> sinstr@acc
-    | 1 -> sinstr@acc
-    | _ -> sinstr::acc
-    ) 
-    []
+//let assemble instrs = instrs |> List.collect sinstrToInt
+
+let assemble instrs = List.foldBack (fun sinstr acc -> (sinstrToInt sinstr) @ acc) instrs []
 
 (* Output the integers in list inss to the text file called fname: *)
 
 let intsToFile (inss : int list) (fname : string) = 
     let text = String.concat " " (List.map string inss)
     System.IO.File.WriteAllText(fname, text);;
+
+
+let e1 = Let("z", 
+              CstI 17, 
+              Prim("+", Var "z", Var "z"));;
+
+intsToFile (assemble (scomp e1 [])) "is1.txt";;
+
+(* Ex 2.5 - disassemble from integers *)
