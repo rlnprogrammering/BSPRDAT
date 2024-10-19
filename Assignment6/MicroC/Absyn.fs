90d2470df029b47c61c49856f7880f94f04488f2
Frederik Lund Rosenlund <frlr@itu.dk>
2024-10-19 13:50:18 +0200

    PreIncrement and PreDecrement

--- Assignment6/MicroC/Absyn.fs
+++ Assignment6/MicroC/Absyn.fs
@@ -23,6 +23,8 @@ and expr =
   | Andalso of expr * expr           (* Sequential and              *)
   | Orelse of expr * expr            (* Sequential or               *)
   | Call of string * expr list       (* Function call f(...)        *)
+  | PreInc of access                 (* C/C++/Java/C# ++i or ++a[e] *)
+  | PreDec of access                 (* C/C++/Java/C# --i or --a[e] *)
                                                                    
 and access =                                                       
   | AccVar of string                 (* Variable access        x    *) 
--- Assignment6/MicroC/Interp.fs
+++ Assignment6/MicroC/Interp.fs
@@ -194,6 +194,14 @@ and eval e locEnv gloEnv store : int * store =
       let (i1, store1) as res = eval e1 locEnv gloEnv store
       if i1<>0 then res else eval e2 locEnv gloEnv store1
     | Call(f, es) -> callfun f es locEnv gloEnv store 
+    | PreInc acc -> 
+      let (loc, store1) = access acc locEnv gloEnv store
+      let res = getSto store1 loc
+      (res, setSto store1 loc (res+1))
+    | PreDec acc -> 
+      let (loc, store1) = access acc locEnv gloEnv store
+      let res = getSto store1 loc
+      (res, setSto store1 loc (res-1))
 
 and access acc locEnv gloEnv store : int * store = 
     match acc with 
