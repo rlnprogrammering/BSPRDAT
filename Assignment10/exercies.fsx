9d64bf23eb82e2db4dcf562a255ac39e5ede3fec
Natalie Clara Petersen <natp@itu.dk>
2024-11-21 14:56:55 +0100

    11.8 (ii)

--- Assignment10/exercies.fsx
+++ Assignment10/exercies.fsx
@@ -80,3 +80,5 @@ let rec prodi xs acc =
 // (i)
 // run (Every(Write(Prim("+", CstI 1, Prim("*", CstI 2, FromTo(1, 4))))));;
 // run (Every(Write(Prim("+", Prim("*", CstI 10, FromTo(2, 4)), FromTo(1, 2)))));;
+// (ii)
+// run Write(Prim("<", CstI 50, Prim("*", CstI 7, FromTo(2, 10))));;
